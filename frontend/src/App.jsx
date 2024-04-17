ca4bb371c96efa1a349c2f31085c2ccc7946a8c7
rishabh-digimantra <leader.akashak@gmail.com>
2024-04-17 17:17:21 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+
+      <p className='text-3xl bg-red-300'>Vite + React</p>
+
+    </div>
+
+  )
+}
+
+export default App
