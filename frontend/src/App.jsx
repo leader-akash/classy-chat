0fad0aa768c8d0303ff6bb1f255b8ce2913c8760
rishabh-digimantra <leader.akashak@gmail.com>
2024-04-18 13:26:14 +0530

    fixed tailwind issue, app in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+
 
 function App() {
 
   return (
-    <div>
+    <div >
 
-      <p className='text-3xl bg-red-300'>Vite + React</p>
+      <p className='text-[25px] font-bold '>Hello World</p>
 
     </div>
 
